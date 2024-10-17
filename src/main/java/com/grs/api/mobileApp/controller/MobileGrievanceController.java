35454f27688581bd21951201f2c43b309b9a071e
Asif <asifpalak@gmail.com>
2024-10-17 16:13:43 +0600

    feat: api for mobile app

---
+++ src/main/java/com/grs/api/mobileApp/controller/MobileGrievanceController.java
@@ -0,0 +1,7 @@
+package com.grs.api.mobileApp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MobileGrievanceController {
+}
---
+++ src/main/java/com/grs/api/mobileApp/service/MobileGrievanceService.java
@@ -0,0 +1,7 @@
+package com.grs.api.mobileApp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MobileGrievanceService {
+}
