3c2c2c1b7059221b848770a14aa0f0c7d5813963
Asif <asifpalak@gmail.com>
2024-10-22 17:34:05 +0600

    submit grievance with login

--- src/main/java/com/grs/core/repo/grs/GrievanceRepo.java
+++ src/main/java/com/grs/core/repo/grs/GrievanceRepo.java
@@ -30,7 +30,8 @@ public interface GrievanceRepo extends JpaRepository<Grievance, Long> {
             "OR g.trackingNumber = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(CONCAT('0', :trx), '০', '0'), '১', '1'), '২', '2'), '৩', '3'), '৪', '4'), '৫', '5'), '৬', '6'), '৭', '7'), '৮', '8'), '৯', '9')")
     List<Grievance> findGrievancesByTrackingNumber(@Param("trx") String trx);
 
-
+    // Find grievances by complainant's user id
+    List<Grievance> findGrievancesByComplainantId(Long id);
 
     @Query(value = "SELECT * FROM grs_only_3.complaints WHERE complainant_id IN ( " +
             "SELECT id FROM grs_only_3.complainants " +
