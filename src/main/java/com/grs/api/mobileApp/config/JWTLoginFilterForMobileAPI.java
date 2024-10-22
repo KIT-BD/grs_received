4f19564e2900f1307ca9493d253c50d4c7b9a00c
Asif <asifpalak@gmail.com>
2024-10-22 11:59:08 +0600

    fixed null pointer exceptions

--- src/main/java/com/grs/api/mobileApp/config/JWTLoginFilterForMobileAPI.java
+++ src/main/java/com/grs/api/mobileApp/config/JWTLoginFilterForMobileAPI.java
@@ -6,9 +6,11 @@ import com.grs.api.config.security.UserDetailsImpl;
 import com.grs.api.mobileApp.dto.DataDTO;
 import com.grs.api.mobileApp.dto.MobileAuthDTO;
 import com.grs.api.mobileApp.dto.MobileResponse;
+import com.grs.api.mobileApp.dto.MobileResponseNoList;
 import com.grs.api.model.UserInformation;
 import com.grs.api.model.response.ErrorDTO;
 import com.grs.core.domain.grs.Complainant;
+import com.grs.core.domain.grs.CountryInfo;
 import com.grs.core.service.ComplainantService;
 import javassist.NotFoundException;
 import org.springframework.http.HttpStatus;
@@ -104,74 +106,67 @@ public class JWTLoginFilterForMobileAPI extends AbstractAuthenticationProcessing
         SecurityContextHolder.getContext().setAuthentication(authToken);
 
         Complainant complainant = complainantService.findOne(userInformation.getUserId());
-        if (complainant == null) {
-            try {
-                throw new NotFoundException("Complainant not found");
-            } catch (NotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        }
 
         DataDTO responseDTO = DataDTO.builder()
                 .user_info(
                         MobileAuthDTO.builder()
                                 .id(complainant.getId())
                                 .name(complainant.getUsername())
-                                .identification_value(Optional.ofNullable(complainant.getIdentificationValue()).orElse(null))
+                                .identification_value(complainant.getIdentificationValue())
                                 .identification_type(Optional.ofNullable(complainant.getIdentificationType()).map(String::valueOf).orElse(null))
-                                .mobile_number(Optional.ofNullable(complainant.getPhoneNumber()).orElse(null))
-                                .email(Optional.ofNullable(complainant.getEmail()).orElse(null))
+                                .mobile_number(complainant.getPhoneNumber())
+                                .email(complainant.getEmail())
                                 .birth_date(Optional.ofNullable(complainant.getBirthDate()).map(String::valueOf).orElse(null))
-                                .occupation(Optional.ofNullable(complainant.getOccupation()).orElse(null))
-                                .educational_qualification(Optional.ofNullable(complainant.getEducation()).orElse(null))
+                                .occupation(complainant.getOccupation())
+                                .educational_qualification(complainant.getEducation())
                                 .gender(Optional.ofNullable(complainant.getGender()).map(String::valueOf).orElse(null))
-                                .username(Optional.ofNullable(complainant.getUsername()).orElse(null))
-                                .nationality_id(Optional.ofNullable(complainant.getCountryInfo()).map(country -> country.getId()).orElse(null))
-                                .present_address_street(Optional.ofNullable(complainant.getPresentAddressStreet()).orElse(null))
-                                .present_address_house(Optional.ofNullable(complainant.getPresentAddressHouse()).orElse(null))
+                                .username(complainant.getUsername())
+                                .nationality_id(Optional.ofNullable(complainant.getCountryInfo()).map(CountryInfo::getId).orElse(null))
+                                .present_address_street(complainant.getPresentAddressStreet())
+                                .present_address_house(complainant.getPresentAddressHouse())
                                 .present_address_division_id(Optional.ofNullable(complainant.getPresentAddressDivisionId()).map(Long::valueOf).orElse(null))
-                                .present_address_division_name_bng(Optional.ofNullable(complainant.getPresentAddressDivisionNameBng()).orElse(null))
-                                .present_address_division_name_eng(Optional.ofNullable(complainant.getPresentAddressDivisionNameEng()).orElse(null))
+                                .present_address_division_name_bng(complainant.getPresentAddressDivisionNameBng())
+                                .present_address_division_name_eng(complainant.getPresentAddressDivisionNameEng())
                                 .present_address_district_id(Optional.ofNullable(complainant.getPresentAddressDistrictId()).map(Long::valueOf).orElse(null))
-                                .present_address_district_name_bng(Optional.ofNullable(complainant.getPresentAddressDistrictNameBng()).orElse(null))
-                                .present_address_district_name_eng(Optional.ofNullable(complainant.getPresentAddressDistrictNameEng()).orElse(null))
+                                .present_address_district_name_bng(complainant.getPresentAddressDistrictNameBng())
+                                .present_address_district_name_eng(complainant.getPresentAddressDistrictNameEng())
                                 .present_address_type_id(Optional.ofNullable(complainant.getPresentAddressTypeId()).map(Long::valueOf).orElse(null))
-                                .present_address_type_name_bng(Optional.ofNullable(complainant.getPresentAddressTypeNameBng()).orElse(null))
-                                .present_address_type_name_eng(Optional.ofNullable(complainant.getPresentAddressTypeNameEng()).orElse(null))
+                                .present_address_type_name_bng(complainant.getPresentAddressTypeNameBng())
+                                .present_address_type_name_eng(complainant.getPresentAddressTypeNameEng())
                                 .present_address_type_value(Optional.ofNullable(complainant.getPresentAddressTypeValue()).map(String::valueOf).orElse(null))
-                                .present_address_postal_code(Optional.ofNullable(complainant.getPresentAddressPostalCode()).orElse(null))
+                                .present_address_postal_code(complainant.getPresentAddressPostalCode())
                                 .is_blacklisted(false)
-                                .permanent_address_street(Optional.ofNullable(complainant.getPermanentAddressStreet()).orElse(null))
-                                .permanent_address_house(Optional.ofNullable(complainant.getPermanentAddressHouse()).orElse(null))
+                                .permanent_address_street(complainant.getPermanentAddressStreet())
+                                .permanent_address_house(complainant.getPermanentAddressHouse())
                                 .permanent_address_division_id(Optional.ofNullable(complainant.getPermanentAddressDivisionId()).map(Long::valueOf).orElse(null))
-                                .permanent_address_division_name_bng(Optional.ofNullable(complainant.getPermanentAddressDivisionNameBng()).orElse(null))
-                                .permanent_address_division_name_eng(Optional.ofNullable(complainant.getPermanentAddressDivisionNameEng()).orElse(null))
+                                .permanent_address_division_name_bng(complainant.getPermanentAddressDivisionNameBng())
+                                .permanent_address_division_name_eng(complainant.getPermanentAddressDivisionNameEng())
                                 .permanent_address_district_id(Optional.ofNullable(complainant.getPermanentAddressDistrictId()).map(Long::valueOf).orElse(null))
-                                .permanent_address_district_name_bng(Optional.ofNullable(complainant.getPermanentAddressDistrictNameBng()).orElse(null))
-                                .permanent_address_district_name_eng(Optional.ofNullable(complainant.getPermanentAddressDistrictNameEng()).orElse(null))
+                                .permanent_address_district_name_bng(complainant.getPermanentAddressDistrictNameBng())
+                                .permanent_address_district_name_eng(complainant.getPermanentAddressDistrictNameEng())
                                 .permanent_address_type_id(Optional.ofNullable(complainant.getPermanentAddressTypeId()).map(Long::valueOf).orElse(null))
-                                .permanent_address_type_name_bng(Optional.ofNullable(complainant.getPermanentAddressTypeNameBng()).orElse(null))
-                                .permanent_address_type_name_eng(Optional.ofNullable(complainant.getPermanentAddressTypeNameEng()).orElse(null))
+                                .permanent_address_type_name_bng(complainant.getPermanentAddressTypeNameBng())
+                                .permanent_address_type_name_eng(complainant.getPermanentAddressTypeNameEng())
                                 .permanent_address_type_value(Optional.ofNullable(complainant.getPermanentAddressTypeValue()).map(String::valueOf).orElse(null))
-                                .permanent_address_postal_code(Optional.ofNullable(complainant.getPermanentAddressPostalCode()).orElse(null))
+                                .permanent_address_postal_code(complainant.getPermanentAddressPostalCode())
                                 .foreign_permanent_address_zipcode(complainant.getForeignPermanentAddressZipCode())
-                                .foreign_permanent_address_state(Optional.ofNullable(complainant.getForeignPermanentAddressState()).orElse(null))
-                                .foreign_permanent_address_city(Optional.ofNullable(complainant.getForeignPermanentAddressCity()).orElse(null))
-                                .foreign_permanent_address_line2(Optional.ofNullable(complainant.getForeignPermanentAddressLine2()).orElse(null))
-                                .foreign_permanent_address_line1(Optional.ofNullable(complainant.getForeignPermanentAddressLine1()).orElse(null))
-                                .foreign_present_address_zipcode(Optional.ofNullable(complainant.getForeignPresentAddressZipCode()).orElse(null))
-                                .foreign_present_address_state(Optional.ofNullable(complainant.getForeignPresentAddressState()).orElse(null))
-                                .foreign_present_address_city(Optional.ofNullable(complainant.getForeignPresentAddressCity()).orElse(null))
-                                .foreign_present_address_line2(Optional.ofNullable(complainant.getForeignPresentAddressLine2()).orElse(null))
-                                .foreign_present_address_line1(Optional.ofNullable(complainant.getForeignPresentAddressLine1()).orElse(null))
+                                .foreign_permanent_address_state(complainant.getForeignPermanentAddressState())
+                                .foreign_permanent_address_city(complainant.getForeignPermanentAddressCity())
+                                .foreign_permanent_address_line2(complainant.getForeignPermanentAddressLine2())
+                                .foreign_permanent_address_line1(complainant.getForeignPermanentAddressLine1())
+                                .foreign_present_address_zipcode(complainant.getForeignPresentAddressZipCode())
+                                .foreign_present_address_state(complainant.getForeignPresentAddressState())
+                                .foreign_present_address_city(complainant.getForeignPresentAddressCity())
+                                .foreign_present_address_line2(complainant.getForeignPresentAddressLine2())
+                                .foreign_present_address_line1(complainant.getForeignPresentAddressLine1())
                                 .is_authenticated(complainant.isAuthenticated())
                                 .created_at(Optional.ofNullable(complainant.getCreatedAt()).map(String::valueOf).orElse(null))
                                 .modified_at(null)
                                 .created_by(Optional.ofNullable(complainant.getCreatedBy()).map(String::valueOf).orElse(null))
                                 .modified_by(Optional.ofNullable(complainant.getModifiedBy()).map(String::valueOf).orElse(null))
                                 .status(Optional.ofNullable(complainant.getStatus()).map(String::valueOf).orElse(null))
-                                .present_address_country_id(Optional.ofNullable(complainant.getPresentAddressCountryId()).orElse(null))
-                                .permanent_address_country_id(Optional.ofNullable(complainant.getPermanentAddressCountryId()).orElse(null))
+                                .present_address_country_id(complainant.getPresentAddressCountryId())
+                                .permanent_address_country_id(complainant.getPermanentAddressCountryId())
                                 .blacklister_office_id(null)
                                 .blacklister_office_name(null)
                                 .blacklist_reason(null)
@@ -198,21 +193,27 @@ public class JWTLoginFilterForMobileAPI extends AbstractAuthenticationProcessing
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request,
                                               HttpServletResponse response,
-                                              AuthenticationException failed) throws IOException, ServletException {
+                                              AuthenticationException failed) throws IOException {
 
-        String[] message = failed.getMessage().split(" ");
-        String lastMessage = message[message.length - 1];
+//        String message = failed.getMessage();
+//
+//        if (message == null) {
+//            message = "Authentication failed due to an unknown error";
+//        }
 
-        ErrorDTO error  = ErrorDTO.builder()
-                .message("")
-                .status(HttpStatus.UNAUTHORIZED.value())
+        MobileResponseNoList error  = MobileResponseNoList.builder()
+                .status("error")
+                .data("Wrong username or password")
                 .build();
 
-        if (lastMessage.compareTo("credentials") == 0) {
-            error.setMessage("Username or Password is incorrect");
-        } else if (lastMessage.compareTo("disabled") == 0) {
-            error.setMessage("User is disabled");
-        }
+//        if (message.toLowerCase().contains("bad credentials")) {
+//            error.setMessage("Username or Password is incorrect");
+//        } else if (message.toLowerCase().contains("disabled")) {
+//            error.setMessage("User is disabled");
+//        } else {
+//            error.setMessage("Authentication failed");
+//        }
+
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.addHeader("content-type", "application/json;charset=UTF-8");
         ObjectMapper mapper = new ObjectMapper();
