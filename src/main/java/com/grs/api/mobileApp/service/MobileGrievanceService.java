95cbb36f2e6eca1bb1797fb4af920867c5c436dc
Asif <asifpalak@gmail.com>
2024-10-23 16:21:44 +0600

    reformatted date with bangla translations

--- src/main/java/com/grs/api/mobileApp/service/MobileGrievanceService.java
+++ src/main/java/com/grs/api/mobileApp/service/MobileGrievanceService.java
@@ -67,7 +67,6 @@ public class MobileGrievanceService {
                 .name(complainant.getName())
                 .email(complainant.getEmail())
                 .officeId(String.valueOf(officeId))
-                //.OfficeLayers("HQ>Division>District")
                 .serviceId(serviceId == null ? String.valueOf(ServiceType.NAGORIK.ordinal()) : serviceId)
                 .submissionDate(String.valueOf(new Date()))
                 .body(description)
@@ -79,7 +78,7 @@ public class MobileGrievanceService {
                 .offlineGrievanceUpload(false)
                 .PhoneNumber(complainant.getPhoneNumber())
                 .isSelfMotivated(true)
-                .SourceOfGrievance(UserType.COMPLAINANT.name())
+                .SourceOfGrievance(String.valueOf(UserType.COMPLAINANT))
                 .user(complainant.getName())
                 .secret(null)
                 .submittedThroughApi(1)
@@ -175,7 +174,6 @@ public class MobileGrievanceService {
         grievanceDTO.setServiceOthers(null);
         grievanceDTO.setIsAnonymous(false);
         grievanceDTO.setServiceType(ServiceType.NAGORIK);
-        //grievanceDTO.setFiles(new ArrayList<>()); // assuming FileDTO objects will be added later
         grievanceDTO.setOfflineGrievanceUpload(false);
         grievanceDTO.setPhoneNumber(null);
         grievanceDTO.setIsSelfMotivated(true);
@@ -193,22 +191,6 @@ public class MobileGrievanceService {
         grievanceDTO.setDistrictId(districtId);
         grievanceDTO.setUpazilaId(upazilaId);
 
-//        // Create GrievanceWithoutLoginRequestDTO
-//        GrievanceWithoutLoginRequestDTO grievanceDTO = new GrievanceWithoutLoginRequestDTO();
-//        grievanceDTO.setOfficeId(officeId);
-//        grievanceDTO.setBody(description);
-//        grievanceDTO.setSubject(subject);
-//        grievanceDTO.setSpProgrammeId(spProgrammeId);
-//        grievanceDTO.setComplainantPhoneNumber(mobileNumber);
-//        grievanceDTO.setName(name);
-//        grievanceDTO.setEmail(email);
-//        grievanceDTO.setDivision(divisionId);
-//        grievanceDTO.setDistrict(districtId);
-//        grievanceDTO.setUpazila(upazilaId);
-//        grievanceDTO.setGrievanceCategory(Integer.parseInt(complaintCategory));
-//        grievanceDTO.setIsAnonymous(true);
-
-        // Handle file uploads
         if (files != null && !files.isEmpty()) {
             FileContainerDTO fileContainerDTO = storageService.storeFileNew(principal, files.toArray(new MultipartFile[0]));
             List<FileBaseDTO> fileBaseDTOList = fileContainerDTO.getFiles();
@@ -234,84 +216,4 @@ public class MobileGrievanceService {
     public List<Grievance> findGrievancesByUser(Long id){
         return grievanceRepo.findGrievancesByComplainantId(id);
     }
-
-//    public Grievance submitGrievance(GrievanceRequestDTO grievanceRequestDTO){
-//        return submitGrievance(null, grievanceRequestDTO);
-//    }
-
-//    public Grievance submitGrievance(UserInformation userInformation, GrievanceRequestDTO grievanceRequestDTO) {
-//        boolean isGrsUser = false;
-//        boolean offlineGrievanceUploaded = false;
-//        Long uploaderGroOfficeUnitOrganogramId = null;
-//        Long userId;
-//
-//        if (userInformation == null) {
-//            userId = 0L;
-//            isGrsUser = true;
-//        } else {
-//            isGrsUser = userInformation.getUserType().equals(UserType.COMPLAINANT);
-//            userId = isGrsUser ? userInformation.getUserId() : userInformation.getOfficeInformation().getEmployeeRecordId();
-//        }
-//
-//        boolean isAnonymous = (grievanceRequestDTO.getIsAnonymous() != null && grievanceRequestDTO.getIsAnonymous());
-//        if (grievanceRequestDTO.getOfflineGrievanceUpload() != null && grievanceRequestDTO.getOfflineGrievanceUpload()) {
-//            offlineGrievanceUploaded = true;
-//            uploaderGroOfficeUnitOrganogramId = userInformation != null && userInformation.getOfficeInformation() != null ? userInformation.getOfficeInformation().getOfficeUnitOrganogramId() : null;
-//            if (!isAnonymous) {
-//                Complainant complainant = this.complainantService.findComplainantByPhoneNumber(grievanceRequestDTO.getPhoneNumber());
-//                userId = complainant.getId();
-//            }
-//            isGrsUser = true;
-//        }
-//
-//        Long officeId = Long.valueOf(grievanceRequestDTO.getOfficeId());
-//        CitizenCharter citizenCharter = null;
-//
-//        // Check for null or empty serviceId before parsing
-//        if (grievanceRequestDTO.getServiceId() != null && !grievanceRequestDTO.getServiceId().isEmpty()) {
-//            citizenCharter = this.citizenCharterDAO.findByOfficeIdAndServiceId(officeId, Long.valueOf(grievanceRequestDTO.getServiceId()));
-//        }
-//
-//        GrievanceCurrentStatus currentStatus = (officeId == 0 ? GrievanceCurrentStatus.CELL_NEW : GrievanceCurrentStatus.NEW);
-//        MediumOfSubmission medium = MediumOfSubmission.ONLINE;
-//
-//        if (currentStatus.equals(GrievanceCurrentStatus.NEW) && grievanceRequestDTO.getOfflineGrievanceUpload() != null && grievanceRequestDTO.getOfflineGrievanceUpload()) {
-//            medium = MediumOfSubmission.CONVENTIONAL_METHOD;
-//        } else if (currentStatus.equals(GrievanceCurrentStatus.NEW) && grievanceRequestDTO.getIsSelfMotivated() != null && grievanceRequestDTO.getIsSelfMotivated()) {
-//            medium = MediumOfSubmission.SELF_MOTIVATED_ACCEPTANCE;
-//        }
-//
-//        Grievance grievance = Grievance.builder()
-//                .details(grievanceRequestDTO.getBody())
-//                .subject(grievanceRequestDTO.getSubject())
-//                .officeId(officeId)
-//                .officeLayers(grievanceRequestDTO.getOfficeLayers())
-//                .serviceOrigin(citizenCharter == null ? null : citizenCharter.getServiceOrigin())
-//                .serviceOriginBeforeForward(citizenCharter == null ? null : citizenCharter.getServiceOrigin())
-//                .grsUser(isGrsUser)
-//                .complainantId(isAnonymous ? 0L : userId)
-//                .grievanceType(grievanceRequestDTO.getServiceType())
-//                .grievanceCurrentStatus(currentStatus)
-//                .isAnonymous(isAnonymous)
-//                .trackingNumber(StringUtil.isValidString(grievanceRequestDTO.getServiceTrackingNumber()) ? grievanceRequestDTO.getServiceTrackingNumber() : null)
-//                .otherService(grievanceRequestDTO.getServiceOthers())
-//                .otherServiceBeforeForward(grievanceRequestDTO.getServiceOthers())
-//                .serviceReceiver(grievanceRequestDTO.getServiceReceiver())
-//                .serviceReceiverRelation(grievanceRequestDTO.getRelation())
-//                .isOfflineGrievance(offlineGrievanceUploaded)
-//                .uploaderOfficeUnitOrganogramId(uploaderGroOfficeUnitOrganogramId)
-//                .isSelfMotivatedGrievance(grievanceRequestDTO.getIsSelfMotivated() != null && grievanceRequestDTO.getIsSelfMotivated())
-//                .sourceOfGrievance(grievanceRequestDTO.getSourceOfGrievance())
-//                .complaintCategory(grievanceRequestDTO.getGrievanceCategory())
-//                .mediumOfSubmission(medium.name())
-//                .spProgrammeId(StringUtil.isValidString(grievanceRequestDTO.getSpProgrammeId()) ? Integer.parseInt(grievanceRequestDTO.getSpProgrammeId()) : null)
-//                .geoDivisionId(StringUtil.isValidString(grievanceRequestDTO.getDivision()) ? Integer.parseInt(grievanceRequestDTO.getDivision()) : null)
-//                .geoDistrictId(StringUtil.isValidString(grievanceRequestDTO.getDistrict()) ? Integer.parseInt(grievanceRequestDTO.getDistrict()) : null)
-//                .geoUpazilaId(StringUtil.isValidString(grievanceRequestDTO.getUpazila()) ? Integer.parseInt(grievanceRequestDTO.getUpazila()) : null)
-//                .build();
-//
-//        grievance.setStatus(true);
-//        grievance = grievanceRepo.save(grievance);
-//        return grievance;
-//    }
 }
